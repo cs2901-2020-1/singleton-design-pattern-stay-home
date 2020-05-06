e12ac6b31d502259a9c04a436b2feb5523f0e38b
Juan Navarro <juan.navarro@utec.edu.pe>
2020-05-06 13:35:17 -0500

    threaded main created

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
 	public static void main(String[] args){ 
-        int n = 8; // Number of threads 
+        int n = 8; // define number of threads and call a for loop for parallel execution
         for (int i=0; i<8; i++){ 
             Thread singletonThreads = new Thread(new MultiThread()); 
             singletonThreads.start(); 
