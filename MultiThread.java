7fdb9f63e1d9ae18ab55408fba19618d49013217
Juan Navarro <juan.navarro@utec.edu.pe>
2020-05-06 13:41:35 -0500

    multithread done

---
+++ MultiThread.java
@@ -0,0 +1,14 @@
+class MultiThread extends Thread{
+
+  public void run(){
+    // executes multi thread
+    try{
+      System.out.println("Thread " + Thread.currentThread().getId()+ " is running...\n");
+      ChocolateBoiler choco = ChocolateBoiler.getInstance();
+
+    }
+    catch(Exception e){
+      System.out.println("Exception is caught :c");
+    }
+  }
+}
